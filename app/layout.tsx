b7309a3d6f99da6839ac13fefc849be3903f4ccb
soms-dotcom <bitsombuds@gmail.com>
2025-11-27 19:21:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   title: "SwasthAI",
   description: "Health Schemes and Insurances",
   icons: {
-    icon: "/healthfav.png", 
+    icon: "/healthfav.ico", 
   },
 };
 
