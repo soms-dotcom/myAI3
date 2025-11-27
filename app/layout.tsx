2808eef73050433b2a207d43bd8cd95010ea2b5a
soms-dotcom <bitsombuds@gmail.com>
2025-11-27 16:31:24 +0530

    Update layout.tsx - tittle

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MyAI3",
-  description: "MyAI3",
+  title: "Health Buddy",
+  description: "My Schemes and Insurances",
 };
 
 export default function RootLayout({
