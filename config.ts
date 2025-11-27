7e3c2c22c15de07c515bffc9b2d485a458236690
soms-dotcom <bitsombuds@gmail.com>
2025-11-27 18:59:50 +0530

    Update config.ts welcome

--- config.ts
+++ config.ts
@@ -33,7 +33,7 @@ export const DATE_AND_TIME = getDateAndTime();
 export const AI_NAME = "SwasthAI";
 export const OWNER_NAME = "Sushant";
 
-export const WELCOME_MESSAGE = `Welcome to ${AI_NAME}, I’m here to help you understand health insurance and schemes in India. How can I support you today? an AI assistant created by ${OWNER_NAME}.`
+export const WELCOME_MESSAGE = `Welcome to ${AI_NAME}, Ask me anything about Indian health insurance and schemes. An AI assistant created by ${OWNER_NAME}.`
 
 export const CLEAR_CHAT_TEXT = "New";
 
